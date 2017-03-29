cbde49b208b8ab099077c99e34d0e0c4e2aef0a6
quadrat1c <w4rd3nbot@gmail.com>
2017-03-29 09:32:06 -0700

    Update notes.js

--- notes.js
+++ notes.js
@@ -4,3 +4,21 @@
 // HEAL 32 (16/16) Move/Heal
 [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK]
 [MOVE,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL,HEAL,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL]
+
+/** Private Server OCS Bot **/
+
+// custom.task.taskName.js
+// custom.creep.behaviour.behaviourName.js
+// virial.mainInjection.js (LOAD/INITIALIZE)
+
+// Basic task implementation:
+const mod = {};
+module.exports = mod;
+mod.extend = function() {
+	try {
+		Task.installTask('taskName');
+		Creep.behaviour.behaviourName = load('creep.behaviour.behaviourName');
+	} catch(e) {
+		console.log(e.stack || e);
+	}
+};
